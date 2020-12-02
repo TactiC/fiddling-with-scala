9eaf18fc31bf10297faf798e89053eeebded0c76
ronalddehaan <rodhaan@wehkamp.nl>
2020-12-02 09:26:18 +0100

    Advent of code day 1

--- src/test/scala/nl/ronalddehaan/advent/Day1.scala
+++ src/test/scala/nl/ronalddehaan/advent/Day1.scala
@@ -6,15 +6,26 @@ object Day1 extends App {
 
   val isTwentyTwenty: (Int, Int) => Boolean = (a,b) => a + b == 2020
 
-  val source = Source.fromFile("numbers.txt")
-  val numbers = source.getLines().toList
-  val (threeDigits, fourDigits) = numbers.partition(p => p.length > 3)
+  val (fourDigits, threeDigits) = {
+    val source = Source.fromFile("numbers.txt")
+    val (a, b) = source.getLines().toList.partition(_.length > 3)
+    source.close()
+    (a.map(_.toInt), b.map(_.toInt))
+  }
 
-  val foo = threeDigits.flatMap { three =>
-    fourDigits.collect {
-      case four if (isTwentyTwenty(four.toInt, three.toInt)) => four.toInt * three.toInt
+  val result = fourDigits.flatMap { three =>
+    threeDigits.collect {
+      case four if isTwentyTwenty(four, three) => four * three
     }
   }
-  println(s"Solution of day 1 is ${foo.head}")
-  source.close()
+
+  val part2 = {
+    val source = Source.fromFile("numbers.txt")
+    val numbers = source.getLines().toList.map(_.toInt)
+    source.close()
+    for (a <- numbers; b <- numbers; c <- numbers if a + b + c == 2020) yield (a * b * c)
+  }
+
+  println(s"Solution of day 1 is ${result.head}")
+  println(s"Solution of day 1 part II is ${part2.head}")
 }
